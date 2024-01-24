d5c8e90ccc3968e92f6fc628983824d8e6b127fe
adithyanmadhi <154506917+adithyanmadhi@users.noreply.github.com>
2024-01-24 12:53:36 +0530

    Upload algorithm file

---
+++ project-euler/task1/algorithm.js
@@ -0,0 +1,4 @@
+Initialize: Set sum to 0.
+Loop: Iterate through each number i from 1 to limit - 1.
+If i is divisible by 3 or divisible by 5, add i to sum.
+Return: The final value of sum is the sum of multiples of 3 or 5 below the given limit.
