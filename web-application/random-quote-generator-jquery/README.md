73d2bd2dfcf06a64ed756fd200fc4bdd73db5e5f
adithyanmadhi <154506917+adithyanmadhi@users.noreply.github.com>
2024-02-07 17:36:50 +0530

    feat: Add a quote generator with jQuery (#28)

---
+++ web-application/random-quote-generator-jquery/README.md
@@ -0,0 +1,5 @@
+# Javascript web app / web site exercise beginner practise.Using  jQuery.
+
+A simple Random Quote Generator
+
+Problem Link: https://mahendrankannan.atlassian.net/browse/ON-29
---
+++ web-application/random-quote-generator-jquery/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    const quotes = [
+        "The only way to do great work is to love what you do. - Steve Jobs",
+        "Believe you can and you're halfway there. - Theodore Roosevelt",
+        "Life is what happens when you're busy making other plans. - John Lennon",
+        "You miss 100% of the shots you don't take. - Wayne Gretzky",
+        "It does not matter how slowly you go as long as you do not stop. - Confucius",
+        "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+        "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+    ];
+
+    const quoteElement = $("#quote");
+    const newQuoteBtn = $("#new-quote-btn");
+
+    function getRandomQuote() {
+        return quotes[Math.floor(Math.random() * quotes.length)];
+    }
+
+    function displayQuote() {
+        quoteElement.text(getRandomQuote());
+    }
+
+    newQuoteBtn.click(displayQuote);
+
+    displayQuote();
+});
